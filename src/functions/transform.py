a38985554c0663cb03b8b79334a4e722f3d92f39
Your Name <swietonp@icloud.com>
2024-04-14 12:36:21 +0200

    function: transform_wgs84_to_cs92

--- src/functions/transform.py
+++ src/functions/transform.py
@@ -11,3 +11,15 @@ def transform_cs92_to_wgs84(x: float, y: float) -> tuple:
     transformer = Transformer.from_crs("EPSG:2180", "EPSG:4326", always_xy=True)
     lon, lat = transformer.transform(x, y)
     return lon, lat
+
+
+def transform_wgs84_to_cs92(lon: float, lat: float) -> tuple:
+    """
+    Transformer that will convert (EPSG:4326 (WGS 84)) to (EPSG:2180 (CS92))
+    :param lon: longitude parameter of geo coordinates (EPSG:4326 (WGS 84))
+    :param lat: latitude parameter of geo coordinates (EPSG:4326 (WGS 84))
+    :return: x and y (EPSG:2180 (CS92))
+    """
+    transformer = Transformer.from_crs("EPSG:4326", "EPSG:2180", always_xy=True)
+    x, y = transformer.transform(lon, lat)
+    return x, y
